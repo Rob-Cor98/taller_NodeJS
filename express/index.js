37e42b3d3a06bc8c08fe3110165386ce0262cbd3
Roberto <rob.cor98829@gmail.com>
2019-09-26 17:07:29 -0500

    urls

--- express/index.js
+++ express/index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World btchs!');
+let array = new Array();
+var a = {};
+
+app.get('/:id', (req, res) => {
+    //console.log(req.params.id);
+    if(req.url != "/favicon.ico"){
+        array.push(req.params.id);
+        console.log(array);
+        res.send('Hello World btchs!');
+    }
 });
   
+app.get('/:nombre/:edad', (req, res) => {
+    if(req.url != "/favicon.ico"){
+        a.nombre = req.params.nombre;
+        a.edad = req.params.edad;
+        console.log(a);
+        res.send('Hello World btchs!');
+    }
+});
+
 app.listen(3000, () => {
     console.log('Server on port 3000!');
 });
